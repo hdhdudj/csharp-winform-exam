e826f3c77b42086393ea92e12c0a7a5c6a11d558
hdhdudj <dnjswbf@gmail.com>
2020-01-09 12:05:43 +0900

    c샵 윈폼 예제

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Name = new System.Windows.Forms.Button();
+            this.txt_Message = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Name
+            // 
+            this.btn_Name.Location = new System.Drawing.Point(267, 192);
+            this.btn_Name.Name = "btn_Name";
+            this.btn_Name.Size = new System.Drawing.Size(282, 88);
+            this.btn_Name.TabIndex = 0;
+            this.btn_Name.Text = "버튼";
+            this.btn_Name.UseVisualStyleBackColor = true;
+            // 
+            // txt_Message
+            // 
+            this.txt_Message.Location = new System.Drawing.Point(120, 110);
+            this.txt_Message.Name = "txt_Message";
+            this.txt_Message.Size = new System.Drawing.Size(566, 35);
+            this.txt_Message.TabIndex = 1;
+            this.txt_Message.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(267, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(282, 84);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_Message);
+            this.Controls.Add(this.btn_Name);
+            this.Name = "Form";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Name;
+        private System.Windows.Forms.TextBox txt_Message;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading; // 추가된 namespace
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+
+            this.btn_Name.Click += BtnClick_Text;
+            this.button1.Click += BtnClick2;
+        }
+
+        public void BtnClick_Text(object sender, EventArgs e)
+        {
+            Thread thread1 = new Thread(new ThreadStart(Calc));
+            thread1.Start();
+   }
+
+        delegate void aCall(int a);
+
+        private void Calc()
+        {
+            int a = 0;
+            while (true)
+            {
+                txt_Message.Invoke(new aCall(setText), new object[] {a});
+                a++;
+            }
+        }
+
+        private void setText(int a)
+        {
+            txt_Message.Text = a.ToString();
+            txt_Message.Refresh();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void BtnClick2(object sender, EventArgs e)
+        {
+            //Thread thread = new Thread(new ThreadStart(delegate ()
+            //{
+                    string message = string.Empty;
+
+                    message = string.Format(txt_Message.Text);
+                    MessageBox.Show(message);
+            //}));
+            //thread.Start();
+
+        }
+    }
+}
